82be9e38f976328c7e9191dc6332d61c9504f31f
Libertox <damians34@interia.pl>
2024-02-20 13:09:17 +0100

    Add ISelectable interface
    Implemented the ISelectable interface to aid in detecting selectable objects.

---
+++ zadanie-testowe/Assets/Scripts/Agent/ISelectable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TestTask
+{
+    public interface ISelectable
+    {
+        public void Select();
+        public void Deselect();
+    }
+}
+
+
