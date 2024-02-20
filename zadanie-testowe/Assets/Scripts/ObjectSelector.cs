57b8a1da276f20c6c923bcce2a9690e87d3e3737
Libertox <damians34@interia.pl>
2024-02-20 21:29:11 +0100

    Add ObjectSelector script
    Implemented the ObjectSelector script to enable the selection of objects on the map.

---
+++ zadanie-testowe/Assets/Scripts/ObjectSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestTask
+{
+    public class ObjectSelector: MonoBehaviour
+    {
+        private void Start()
+        {
+            GameInput.Instance.OnMouseClicked += GameInput_OnMouseClicked;
+        }
+
+        private void GameInput_OnMouseClicked(object sender, GameInput.OnMouseClickedEventArgs e)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(e.mousePosition);
+            float maxDistance = 100f;
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, maxDistance))
+            {
+                if(raycastHit.transform.TryGetComponent(out ISelectable selectable))
+                {
+                    selectable.Select();
+                }
+            }
+
+        }
+    }
+}
