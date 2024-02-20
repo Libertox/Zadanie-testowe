747eced73f4c0e637dfece15e26378a64ff42695
Libertox <damians34@interia.pl>
2024-02-20 13:22:52 +0100

    Create Agent script
    Implemented the Agent script responsible for managing game agents.

---
+++ zadanie-testowe/Assets/Scripts/Agent/Agent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestTask
+{
+    public class Agent : MonoBehaviour, ISelectable
+    {
+        public static event EventHandler<OnSelectedEventArgs> OnSelected;
+        public static event EventHandler OnDeselected;
+
+        public class OnSelectedEventArgs: EventArgs
+        {
+            public string name;
+            public int hp;
+        }
+
+        [SerializeField] private float speed;
+
+        [SerializeField] private string agentName;
+
+        private int hp;
+
+        public void Select()
+        {
+            Debug.Log("Select");
+            OnSelected?.Invoke(this, new OnSelectedEventArgs
+            {
+                name = agentName,
+                hp = hp
+            });
+        }
+
+        public void Deselect()
+        {
+            Debug.Log("Deselect");
+            OnDeselected?.Invoke(this, EventArgs.Empty);
+        }
+
+       
+    }
+}
