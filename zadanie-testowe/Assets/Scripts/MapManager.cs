55efe555d2a9a38093e17af16fc111c77a445f67
Libertox <damians34@interia.pl>
2024-02-20 10:49:43 +0100

    Add MapManager script for managing the game map

---
+++ zadanie-testowe/Assets/Scripts/MapManager.cs
@@ -0,0 +1,51 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestTask
+{
+    public class MapManager : MonoBehaviour
+    {
+        public static MapManager Instance { get; private set; }
+
+        [SerializeField] private float mapWidth;
+        [SerializeField] private float mapHeight;
+
+        [SerializeField] private Transform map;
+
+
+        private void Awake()
+        {
+            if (!Instance)
+                Instance = this;
+            else
+                Destroy(gameObject);
+        }
+        private void Start()
+        {
+            SetupMapDimension(); 
+        }
+
+        private void SetupMapDimension()
+        {
+            map.localScale = new Vector3(mapHeight, map.localScale.y, mapWidth);        
+        }
+
+        private Vector3 GetRandomPositionWithinMap()
+        {
+            float maxPositionX = map.transform.position.x + mapHeight * 0.5f;
+            float minPositionX = map.transform.position.x - mapHeight * 0.5f;
+
+            float posX = UnityEngine.Random.Range(minPositionX, maxPositionX);
+
+            float maxPositionZ = map.transform.position.z + mapWidth * 0.5f;
+            float minPositionZ = map.transform.position.z - mapWidth * 0.5f;
+
+            float posZ = UnityEngine.Random.Range(minPositionZ, maxPositionZ);
+
+            return new Vector3(posX, map.transform.position.y, posZ);
+        }
+
+    }
+}
