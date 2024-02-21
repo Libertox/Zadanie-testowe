c7ee967da82ea5f011156f243cae9fd3e61f0558
Libertox <damians34@interia.pl>
2024-02-21 08:46:26 +0100

    Create AgentUI script
    Implemented the AgentUIScript to show the name and health points of selected agents.

---
+++ zadanie-testowe/Assets/Scripts/UI/AgentUI.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace TestTask
+{
+    public class AgentUI: MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI agentNameText;
+
+        [SerializeField] private List<Transform> agentHearts;
+
+        private void Start()
+        {
+            Agent.OnDeselected += Agent_OnDeselected;
+            Agent.OnSelected += Agent_OnSelected;
+
+            Hide();
+        }
+
+        private void Agent_OnDeselected(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void Agent_OnSelected(object sender, Agent.OnSelectedEventArgs e)
+        {
+            UpdateAgentName(e.name);
+            UpdateAgentHearts(e.hp);
+
+            Show();
+        }
+
+        private void UpdateAgentName(string agentName)
+        {
+            agentNameText.SetText(agentName);
+        }
+
+        private void UpdateAgentHearts(int agentHp)
+        {
+            for (int i = 0; i < agentHearts.Count; i++)
+            {
+                agentHearts[i].gameObject.SetActive(i + 1 <= agentHp);
+            }
+        }
+
+        private void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        private void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            Agent.OnDeselected -= Agent_OnDeselected;
+            Agent.OnSelected -= Agent_OnSelected;
+        }
+
+    }
+}
