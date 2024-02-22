8ed513d26f8831d487c4e451a8ed969f61474aaa
Libertox <damians34@interia.pl>
2024-02-22 14:49:53 +0100

    Implement MarkoPolo algorithm

---
+++ zadanie-testowe/Assets/Scripts/MarkoPolo/MarkoPoloAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTask.MarkoPolo
+{
+    public class MarkoPoloAlgorithm
+    {
+         private int startNumber;
+         private int endNumber;
+
+        public MarkoPoloAlgorithm(int startNumber, int endNumber)
+        {
+            this.startNumber = startNumber;
+            this.endNumber = endNumber;
+        }
+
+        public string Execute()
+        {
+            StringBuilder text = new StringBuilder();
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+               text.AppendLine(DetectWord(i));
+            }
+
+            return text.ToString();
+        }
+
+        private string DetectWord(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0) return "MarkoPolo";
+            else if (number % 3 == 0) return "Marko";
+            else if (number % 5 == 0) return "Polo";
+            
+           return number.ToString();
+        }
+        
+    }
+}
---
+++ zadanie-testowe/Assets/Scripts/MarkoPolo/MarkoPoloUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestTask.MarkoPolo
+{
+    public class MarkoPoloUI: MonoBehaviour
+    {
+        private const int StartNumber = 0;
+        private const int EndNumber = 100;
+
+        [SerializeField] private Button button;
+        [SerializeField] private TextMeshProUGUI markoPoloText;
+        [SerializeField] private Transform background;
+
+        private void Awake()
+        {
+            MarkoPoloAlgorithm markoPoloAlgorithm = new MarkoPoloAlgorithm(StartNumber, EndNumber);
+            button.onClick.AddListener(() =>
+            {
+                background.gameObject.SetActive(true);
+                markoPoloText.SetText(markoPoloAlgorithm.Execute());
+            });
+        }
+
+    }
+}
